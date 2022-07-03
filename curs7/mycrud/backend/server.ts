aa5b9bd55e4450c8a76949f763e422d7df247d44
Cosmin-Petrachescu <b00190588t@gmail.com>
2022-07-03 11:03:12 +0300

    rest

--- curs7/mycrud/backend/server.ts
+++ curs7/mycrud/backend/server.ts
@@ -1,15 +1,21 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import path from 'path';
+import { userRouter } from './routes/userRouter';
 
 dotenv.config();
 
 const app: Express = express();
+app.use(express.urlencoded({ extended: true }))
 const port = process.env.PORT;
-
+app.use(cors());
+app.use("/users", userRouter);
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+  //res.send('Express + TypeScript Server!!!!');
+  res.sendFile(path.join(__dirname+'/acasa.html'));
 });
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running ap https://localhost:${port}`);
+  console.log(`[server]: Server is running at https://localhost:${port}`);
 });
